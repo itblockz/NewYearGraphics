ed4e1373e4df8312b5114a1cf4b2c46bfcd1c568
Nontapat <65050434@kmitl.ac.th>
2023-12-19 13:43:10 +0700

    edited line method and change method name triangle to polygon

--- NewYear.java
+++ NewYear.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 public class NewYear extends JPanel {
     private static final int width = 600;
     private static final int height = 600;
+    
     public static void main(String[] args) {
         NewYear m = new NewYear();
         JFrame f = new JFrame();
@@ -29,16 +30,50 @@ public class NewYear extends JPanel {
         curve(g, x, y);
         x = new int[] { 300, 400, 500 };
         y = new int[] { 500, 300, 500 };
-        triangle(g, x, y);
+        polygon(g, x, y);
     }
-    
+
     private void line(Graphics g, int x1, int y1, int x2, int y2) {
-        g.drawLine(x1, y1, x2, y2);
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+
+        int sx = (x1 < x2) ? 1 : -1;
+        int sy = (y1 < y2) ? 1 : -1;
+        boolean isSwap = false;
+
+        if (dy > dx) {
+            int tmp = dx;
+            dx = dy;
+            dy = tmp;
+            isSwap = true;
+        }
+
+        int D = 2 * dy - dx;
+
+        int x = x1;
+        int y = y1;
+        for (int i = 1; i <= dx; i++) {
+            plot(g, x, y);
+
+            if (D >= 0) {
+                if (isSwap)
+                    x += sx;
+                else
+                    y += sy;
+                D -= 2 * dx;
+            }
+            if (isSwap)
+                y += sy;
+            else
+                x += sx;
+
+            D += 2 * dy;
+        }
     }
 
     private void curve(Graphics g, int[] x, int[] y) {
         Graphics2D g2d = (Graphics2D) g;
-        
+
         // Create a cubic Bezier curve
         Path2D path = new Path2D.Double();
         path.moveTo(x[0], y[0]); // Starting point
@@ -47,8 +82,12 @@ public class NewYear extends JPanel {
         // Draw the Bezier curve
         g2d.draw(path);
     }
-    
-    private void triangle(Graphics g, int[] x, int[] y) {
+
+    private void polygon(Graphics g, int[] x, int[] y) {
         g.fillPolygon(x, y, 3);
+    }
+
+    public void plot(Graphics g, int x, int y) {
+        g.fillRect(x, y, 1, 1);
     }
 }
