a447afa2d345f96da061a2f2678f3e62dfd7b07a
Nontapat <65050434@kmitl.ac.th>
2023-12-29 00:52:00 +0700

    add female_left_eye_brow layer

--- HNY.java
+++ HNY.java
@@ -34,7 +34,7 @@ public class HNY extends JPanel {
         super.paint(g);
 
         int start = 0;
-        int end = 27;
+        int end = 62;
         Set<String> layersToDraw = new HashSet<>();
         for (int i = start; i <= end; i++) {
             layersToDraw.add(String.valueOf(i));
