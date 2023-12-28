815e1ea4c5ef38935a80e968293f4794b0040b65
Nontapat <65050434@kmitl.ac.th>
2023-12-29 00:00:42 +0700

    add female_white_cheek layer

--- HNY.java
+++ HNY.java
@@ -34,7 +34,7 @@ public class HNY extends JPanel {
         super.paint(g);
 
         int start = 0;
-        int end = 23;
+        int end = 24;
         Set<String> layersToDraw = new HashSet<>();
         for (int i = start; i <= end; i++) {
             layersToDraw.add(String.valueOf(i));
