187f4f5c1a240d058dffa716d2535c25bf196f90
Nontapat <65050434@kmitl.ac.th>
2023-12-28 23:47:54 +0700

    add female_head layer

--- HNY.java
+++ HNY.java
@@ -34,7 +34,7 @@ public class HNY extends JPanel {
         super.paint(g);
 
         int start = 0;
-        int end = 22;
+        int end = 23;
         Set<String> layersToDraw = new HashSet<>();
         for (int i = start; i <= end; i++) {
             layersToDraw.add(String.valueOf(i));
