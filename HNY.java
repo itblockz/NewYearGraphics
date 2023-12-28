2db70c4764bb8fa4ffb9d9b6b7bda111e281fcf4
Nontapat <65050434@kmitl.ac.th>
2023-12-29 03:58:52 +0700

    add female_left_horn layer

--- HNY.java
+++ HNY.java
@@ -34,7 +34,7 @@ public class HNY extends JPanel {
         super.paint(g);
 
         int start = 0;
-        int end = 67;
+        int end = 68;
         Set<String> layersToDraw = new HashSet<>();
         for (int i = start; i <= end; i++) {
             layersToDraw.add(String.valueOf(i));
