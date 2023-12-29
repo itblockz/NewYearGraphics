41cdcdb7f9f919ed695823dd4a81866883aa2ea0
Nontapat <65050434@kmitl.ac.th>
2023-12-29 16:36:41 +0700

    add female_right_pink_cheek layer

--- HNY.java
+++ HNY.java
@@ -40,7 +40,7 @@ public class HNY extends JPanel {
             layersToDraw.add(String.valueOf(i));
         }
         start = 55;
-        end = 74;
+        end = 75;
         for (int i = start; i <= end; i++) {
             layersToDraw.add(String.valueOf(i));
         }
