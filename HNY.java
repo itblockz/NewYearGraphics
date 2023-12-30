e6f7f2f35a663e2d8e8bf29054b6a122827284a6
Nontapat <65050434@kmitl.ac.th>
2023-12-30 15:24:36 +0700

    add left_firework11 layer

--- HNY.java
+++ HNY.java
@@ -40,7 +40,7 @@ public class HNY extends JPanel {
             layersToDraw.add(String.valueOf(i));
         }
         start = 55;
-        end = 87;
+        end = 88;
         for (int i = start; i <= end; i++) {
             layersToDraw.add(String.valueOf(i));
         }
